275be9100546c855548a04f77278ac8c9daabaad
tknr <info@tknr.com>
2017-12-22 22:55:17 +0900

    2017年 12月 22日 金曜日 22:55:17 JST

--- vminit.sh
+++ vminit.sh
@@ -14,14 +14,14 @@ VBOX=$1
 
 ## http://www.neko.ne.jp/~freewing/software/osx86_hackintosh_virtual_box/
 
-VBoxManage modifyvm $VBOX --cpuidset 00000001 000306a9 00020800 80000201 178bfbff
+VBoxManage modifyvm "$VBOX" --cpuidset 00000001 000306a9 00020800 80000201 178bfbff
 
-VBoxManage setextradata $VBOX "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacBookPro11,3"
-VBoxManage setextradata $VBOX "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
-VBoxManage setextradata $VBOX "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Mac-2BD1B31983FE1663"
-VBoxManage setextradata $VBOX "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
-VBoxManage setextradata $VBOX "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
-VBoxManage setextradata $VBOX "VBoxInternal/Devices/efi/0/Config/DmiSystemSerial" "SERIAL_NO"
+VBoxManage setextradata "$VBOX" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacBookPro11,3"
+VBoxManage setextradata "$VBOX" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+VBoxManage setextradata "$VBOX" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Mac-2BD1B31983FE1663"
+VBoxManage setextradata "$VBOX"" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+VBoxManage setextradata "$VBOX" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+VBoxManage setextradata "$VBOX" "VBoxInternal/Devices/efi/0/Config/DmiSystemSerial" "SERIAL_NO"
 
 ##### 
 ## 0  640 x  480
@@ -30,9 +30,9 @@ VBoxManage setextradata $VBOX "VBoxInternal/Devices/efi/0/Config/DmiSystemSerial
 ## 3 1280 x 1024
 ## 4 1440 x  900
 ## 5 1900 x 1200
-VBoxManage setextradata $VBOX "VBoxInternal2/EfiGopMode" 4
+VBoxManage setextradata "$VBOX" "VBoxInternal2/EfiGopMode" 4
 
-#VBoxManage setextradata $VBOX "CustomVideoMode1" "1920x1080x32"
-#VBoxManage setextradata $VBOX "GUI/CustomVideoMode1" "1920x1080x32"
+#VBoxManage setextradata "$VBOX" "CustomVideoMode1" "1920x1080x32"
+#VBoxManage setextradata "$VBOX" "GUI/CustomVideoMode1" "1920x1080x32"
 
  
